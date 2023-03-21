6af3297d47eff1b8450d4800ca76a1f061dfa115
Artur Rakhimov <kharkiv1c@gmail.com>
2023-03-21 23:05:48 +0100

    dev

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -4,7 +4,7 @@ Should be equal the the height of the header
 ========================================== */
 
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 48) {
+    if ($(window).scrollTop() >= 134) {
         $("nav").addClass("fixed-header");
         $("nav div").addClass("visible-title");
         $("nav img").attr("src", "./images/logo2.jpg");
