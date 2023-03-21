09df258bf4121aad95d5bd29d577d5894f9a2f76
Artur Rakhimov <kharkiv1c@gmail.com>
2023-03-21 20:58:42 +0100

    dev

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -7,8 +7,10 @@ $(window).scroll(function () {
     if ($(window).scrollTop() >= 48) {
         $("nav").addClass("fixed-header");
         $("nav div").addClass("visible-title");
+        $("nav img").attr("src", "./images/logo2.jpg");
     } else {
         $("nav").removeClass("fixed-header");
         $("nav div").removeClass("visible-title");
+        $("nav img").attr("src", "./images/logo1.jpg");
     }
 });
